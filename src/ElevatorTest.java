5e3d60aea027fd1efc3d9b153a406d2154ce1c3b
AhmadReginaldSyahiran <112646010+AhmadReginaldSyahiran@users.noreply.github.com>
2022-10-21 15:20:05 +0700

    Add files via upload

---
+++ src/ElevatorTest.java
@@ -0,0 +1,18 @@
+public class ElevatorTest {
+    public static void main(String[] args) {
+        Elevator myElevator = new Elevator();
+        myElevator.openDoor();
+        myElevator.closeDoor();
+        myElevator.goUP();
+        myElevator.goUP();
+        myElevator.goUP();
+        myElevator.openDoor();
+        myElevator.closeDoor();
+        myElevator.goDown();
+        myElevator.openDoor();
+        myElevator.closeDoor();
+        myElevator.goDown();
+        myElevator.setFloor(myElevator.TOP_FLOOR);
+        myElevator.openDoor();
+    }
+}
